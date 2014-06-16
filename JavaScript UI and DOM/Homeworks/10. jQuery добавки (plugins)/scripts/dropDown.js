4b66e44510c535c106bb01e854d0dcd6f6ff3c8f
Ilia Pendzhurov <iliapenjurov@gmail.com>
2014-06-16 14:05:03 +0300

    Change in homework

--- JavaScript UI and DOM/Homeworks/10. jQuery добавки (plugins)/scripts/dropDown.js
+++ JavaScript UI and DOM/Homeworks/10. jQuery добавки (plugins)/scripts/dropDown.js
@@ -5,23 +5,42 @@ window.onload = function () {
 $.fn.dropdown = function () {
 	var $container = $(this),
 		$dropDownContainer = $('<div>').addClass("dropdown-list-container"),
-		$ul = $('<ul>').addClass('dropdown-list-options'),
-		$li = $('<li>').addClass('dropdown-list-option'),
-		$selectedOption = $('<strong>');
+		$ul = $('<ul>').addClass('dropdown-list-options')
+			.css({ 'list-style-type' : 'none'}),
+		$li = $('<li>').addClass('dropdown-list-option')
+			.css({'background-color' : 'green',
+				 'margin-bottom' : '2px',
+				 'width' : '100px',
+				 'height' : '25px',
+				 'line-height' : '25px',
+           		 'text-align': 'center'
+			}),
+		$label = $('<li>').addClass('dropdown-list-label').text('Select')
+			.css({'background-color' : 'lightgreen',
+				 'margin-bottom' : '2px',
+				 'width' : '100px',
+				 'height' : '25px',
+				 'line-height' : '25px',
+           		 'text-align': 'center'
+			});
 
 	$container.hide();
 
+	$ul.append($label);
 	for (var i = 0, len = $container.children().length; i < len; i++) {
 		createNewLi($container.children()[i]);		
 	};
 
+	$ul.children().hide().first().show().on('click', function () {
+    	$('.dropdown-list-option').slideToggle();
+    });
+
 	$dropDownContainer.append($ul);
-	$dropDownContainer.append($selectedOption);
 	$('body').append($dropDownContainer);
 
 	$dropDownContainer.find(".dropdown-list-option").on("click", function () {
-			selectOption($(this));
-        });
+		selectOption($(this));
+    });
 
 	function createNewLi(item) {
 		var $currentLi = $li.clone();
@@ -36,15 +55,12 @@ $.fn.dropdown = function () {
 			option  = selector.attr('data-value'),
 			currentOption = 'option[value=\'' + (parseInt(option)+1) + '\']';
 
-    	$ul.children().css('font-weight', '');
-        $container.find('.current').removeAttr('selected').removeClass('current');            
+		// $("#dropdown option:selected") is working
+		$("#dropdown option:selected").removeAttr('selected');          
 
-        $container.find(currentOption).attr('selected', 'selected').addClass("current");
-        selector.css('font-weight', '900');
+        $container.find(currentOption).attr('selected', 'selected');
+        $label.text($selectedLi.text());
 
-        $selectedOption.text('Selected option number ' + $container.find(currentOption).text())
-        	.css('margin-left', '20px');
+        $('.dropdown-list-options li:not(.dropdown-list-label)').slideUp('fast');
 	}
 }
-
-
